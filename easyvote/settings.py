c3e0201e66f1abfa3974eaca3cf058c98e0e1755
Nithin <bonagirinithin2002@gmail.com>
2025-04-02 17:35:17 +0530

    Update settings.py

--- easyvote/settings.py
+++ easyvote/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@!w3q1da)_fk7%pd^cgxoa(min=$rnd4oz5dm#ul&m$dgql#(d'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
